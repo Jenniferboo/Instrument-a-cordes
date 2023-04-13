513ef6f7d54e20010273b20820d980289820eeaa
Jenniferboo <bugara.jennifer@gmail.com>
2023-04-13 15:03:58 +0200

    detail1

--- publicView/detailArticleView.php
+++ publicView/detailArticleView.php
@@ -1,33 +1,41 @@
-<!DOCTYPE html>
+<l-></l-><!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Détail article</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+
 </head>
 <body>
     <nav>
         <?php include_once "src/menu.php"?>
     </nav>
 
-<h2></h2>
-<p></p>
-
-<h1></h1>
-<p></p>
-
-<h1></h1>
-<p></p>
-
-<h1></h1>
-<p></p>
-
-<h1></h1>
-<p></p>
-
-<h1></h1>
-<p></p>
+    <section>
+     
+        <article>
+            <h1><?=$item->title?></h1>
+            <h1></h1>
+            <p></p>
+        </article>
+        
+        <article>
+            <h1></h1>
+            <p></p>
+        </article>
+        
+        <article>
+            <h1></h1>
+            <p></p>
+        </article>
+        <article>
+            <h1></h1>
+            <p></p>
+        </article>
+    
+    </section>
 
 
     <footer>
